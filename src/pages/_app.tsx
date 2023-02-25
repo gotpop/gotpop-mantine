9674b7a44f83bf3583f6ece8ea915903dbb4411e
GotPop <gotpop@users.noreply.github.com>
2023-02-25 18:36:06 +0000

    Articles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,33 @@
 import { MantineProvider } from "@mantine/core"
 import type { AppProps } from "next/app"
+import { Bebas_Neue, Montserrat } from "@next/font/google"
+
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bebas"
+})
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: "variable",
+  variable: "--font-montserrat"
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme: "light" }}>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <div className={`${bebas.variable} ${montserrat.variable}`}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "light",
+          fontFamily: "var(--font-montserrat), Verdana, sans-serif",
+          headings: { fontFamily: "var(--font-bebas), Greycliff CF, sans-serif" }
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </div>
   )
 }
