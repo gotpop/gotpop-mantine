7d354a6abd5c8093fcad3a49030b53d1a6e20a54
GotPop <gotpop@users.noreply.github.com>
2023-02-26 13:33:12 +0000

    `Enhancement:` Adding auth

--- src/components/UserInfoIcons/UserInfoIcons.tsx
+++ src/components/UserInfoIcons/UserInfoIcons.tsx
@@ -12,40 +12,31 @@ const useStyles = createStyles((theme) => ({
   }
 }))
 
-interface UserInfoIconsProps {
-  image?: string
-  name?: string
-  email?: string
-}
-
 type Props = {
-  session: {
-    user: UserInfoIconsProps
-  }
+  session: Session
 }
 
 export function UserInfoIcons({ session }: Props) {
   const { user } = session
-  const { image, name, email } = user
   const { classes } = useStyles()
 
   return (
     <Paper>
       <Group noWrap>
-        <Avatar src={image} size={94} radius="md" />
+        <Avatar src={user?.image} size={94} radius="md" />
         <div>
           <Text size="xs" sx={{ textTransform: "uppercase" }} weight={700} color="dimmed">
             User info
           </Text>
 
           <Text size="lg" weight={500} className={classes.name}>
-            {name}
+            {user?.name}
           </Text>
 
           <Group noWrap spacing={10} mt={3}>
             <IconAt stroke={1.5} size={16} className={classes.icon} />
             <Text size="xs" color="dimmed">
-              {email}
+              {user?.email}
             </Text>
           </Group>
         </div>
