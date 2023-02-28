ca646169cd186008e7a6cc0cb3833c4f757abd22
GotPop <gotpop@users.noreply.github.com>
2023-02-28 12:27:55 +0000

    `Enhancement:` Updating API

--- src/pages/api/mission.ts
+++ src/pages/api/mission.ts
@@ -5,7 +5,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '@/lib/prisma'
 import { Mission } from '@prisma/client'
 
-export default async function handleMisson(req: NextApiRequest, res: NextApiResponse) {
+export default async function missionHandler(req: NextApiRequest, res: NextApiResponse) {
     const session = await getServerSession(req, res, authOptions)
 
     if (req.method !== 'POST' && req.method !== 'GET') {
@@ -17,13 +17,22 @@ export default async function handleMisson(req: NextApiRequest, res: NextApiResp
     }
 
     const { user } = session
+    const sessionEmail = user?.email ? user?.email : undefined
+
+    if (req.method === 'GET') {
+        const makeCartItem = await prisma.mission.findUnique({
+            where: { userId: sessionEmail }
+        })
+
+        return res.status(200).json(makeCartItem)
+    }
 
     if (req.method === 'POST') {
         const { body } = req
         const { username, password, name, email, website, github, missionType }: Mission = body
 
         const makeMissionItem = await prisma.mission.upsert({
-            where: { userId: user?.email ? user?.email : undefined },
+            where: { userId: sessionEmail },
             update: {
                 username,
                 password,
@@ -34,7 +43,7 @@ export default async function handleMisson(req: NextApiRequest, res: NextApiResp
                 missionType,
             },
             create: {
-                username: username,
+                username,
                 password,
                 name,
                 email,
@@ -42,7 +51,7 @@ export default async function handleMisson(req: NextApiRequest, res: NextApiResp
                 github,
                 missionType,
                 user: {
-                    connect: { email: user?.email as string },
+                    connect: { email: sessionEmail },
                 },
             },
         })
