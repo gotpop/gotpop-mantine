2a02e175d7090dfa19b53d57a7911c52b4d82851
GotPop <gotpop@users.noreply.github.com>
2023-03-02 18:12:33 +0000

    `Task:` Formatting & structure step2

--- src/components/MultiStepForm/Step2/Step2.tsx
+++ src/components/MultiStepForm/Step2/Step2.tsx
@@ -1,49 +1,11 @@
 import { Title, Text, SimpleGrid, Checkbox } from "@mantine/core"
 import { UseFormReturnType } from "@mantine/form"
+import { contactsData } from "./data"
 // import { useStyles } from "../useStyles"
 
 type Props = {
   form: UseFormReturnType<any>
 }
-
-const contactsData = [
-  {
-    id: "1",
-    active: false,
-    title: "Your accountant",
-    description: "Core components library: inputs, buttons, overlays, etc."
-  },
-  {
-    id: "2",
-    active: false,
-    title: "The admin of your Mincraft server",
-    description: "Core components library: inputs, buttons, overlays, etc."
-  },
-  {
-    id: "3",
-    active: false,
-    title: "Your Uber Eats driver",
-    description: "Core components library: inputs, buttons, overlays, etc."
-  },
-  {
-    id: "4",
-    active: false,
-    title: "Tescos self service checkout",
-    description: "Core components library: inputs, buttons, overlays, etc."
-  },
-  {
-    id: "5",
-    active: false,
-    title: "The guy who aproved your pr with a rocket emoji",
-    description: "Core components library: inputs, buttons, overlays, etc."
-  },
-  {
-    id: "6",
-    active: false,
-    title: "@mantine/core",
-    description: "Core components library: inputs, buttons, overlays, etc."
-  }
-]
 
 export function Step2({ form }: Props) {
   // const { classes } = useStyles()
---
+++ src/components/MultiStepForm/Step2/data.ts
@@ -0,0 +1,38 @@
+export const contactsData = [
+    {
+        id: "1",
+        active: false,
+        title: "Your accountant",
+        description: "Core components library: inputs, buttons, overlays, etc."
+    },
+    {
+        id: "2",
+        active: false,
+        title: "The admin of your Mincraft server",
+        description: "Core components library: inputs, buttons, overlays, etc."
+    },
+    {
+        id: "3",
+        active: false,
+        title: "Your Uber Eats driver",
+        description: "Core components library: inputs, buttons, overlays, etc."
+    },
+    {
+        id: "4",
+        active: false,
+        title: "Tescos self service checkout",
+        description: "Core components library: inputs, buttons, overlays, etc."
+    },
+    {
+        id: "5",
+        active: false,
+        title: "The guy who aproved your pr with a rocket emoji",
+        description: "Core components library: inputs, buttons, overlays, etc."
+    },
+    {
+        id: "6",
+        active: false,
+        title: "@mantine/core",
+        description: "Core components library: inputs, buttons, overlays, etc."
+    }
+]
