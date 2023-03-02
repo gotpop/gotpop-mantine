8691c7edaaaa64d2cce93ac0f21fd1e1a906b4c7
GotPop <gotpop@users.noreply.github.com>
2023-03-02 18:15:20 +0000

    `Task:` Formatting & structure step3

--- src/components/MultiStepForm/Step3/Step3.tsx
+++ src/components/MultiStepForm/Step3/Step3.tsx
@@ -1,69 +1,10 @@
 import { Paper, Radio, SimpleGrid } from "@mantine/core"
+import { logoData, taglineData, backgroundData } from "./data"
 // import { useStyles } from "../useStyles"
 
 type Props = {
   form: any
 }
-
-const logoData = [
-  {
-    id: "1",
-    label: "Logo 1",
-    value: "logo1"
-  },
-  {
-    id: "2",
-    label: "Logo 2",
-    value: "logo2"
-  },
-  {
-    id: "3",
-    label: "Logo 3",
-    value: "logo3"
-  }
-]
-
-const taglineData = [
-  {
-    id: "1",
-    active: false,
-    label: "Tagline 1",
-    value: "tagline1"
-  },
-  {
-    id: "2",
-    active: false,
-    label: "Tagline 2",
-    value: "tagline2"
-  },
-  {
-    id: "3",
-    active: false,
-    label: "Tagline 3",
-    value: "tagline3"
-  }
-]
-
-const backgroundData = [
-  {
-    id: "1",
-    active: false,
-    label: "Background 1",
-    value: "background1"
-  },
-  {
-    id: "2",
-    active: false,
-    label: "Background 2",
-    value: "background2"
-  },
-  {
-    id: "3",
-    active: false,
-    label: "Background 3",
-    value: "background3"
-  }
-]
 
 export function Step3({ form }: Props) {
   // const { classes } = useStyles()
---
+++ src/components/MultiStepForm/Step3/data.ts
@@ -0,0 +1,59 @@
+export const logoData = [
+    {
+        id: "1",
+        label: "Logo 1",
+        value: "logo1"
+    },
+    {
+        id: "2",
+        label: "Logo 2",
+        value: "logo2"
+    },
+    {
+        id: "3",
+        label: "Logo 3",
+        value: "logo3"
+    }
+]
+
+export const taglineData = [
+    {
+        id: "1",
+        active: false,
+        label: "Tagline 1",
+        value: "tagline1"
+    },
+    {
+        id: "2",
+        active: false,
+        label: "Tagline 2",
+        value: "tagline2"
+    },
+    {
+        id: "3",
+        active: false,
+        label: "Tagline 3",
+        value: "tagline3"
+    }
+]
+
+export const backgroundData = [
+    {
+        id: "1",
+        active: false,
+        label: "Background 1",
+        value: "background1"
+    },
+    {
+        id: "2",
+        active: false,
+        label: "Background 2",
+        value: "background2"
+    },
+    {
+        id: "3",
+        active: false,
+        label: "Background 3",
+        value: "background3"
+    }
+]
