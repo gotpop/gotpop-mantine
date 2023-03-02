47b73c4aa1c50cddc3c3b7f04bcc39587ff6b91e
GotPop <gotpop@users.noreply.github.com>
2023-03-02 18:15:27 +0000

    `Task:` Formatting & structure step4

--- src/components/MultiStepForm/Step4/Step4.tsx
+++ src/components/MultiStepForm/Step4/Step4.tsx
@@ -1,27 +1,10 @@
 import { Code, Container, Paper, Radio, TextInput } from "@mantine/core"
+import { finalWishData } from "./data"
 // import { useStyles } from "../useStyles"
 
 type Props = {
   form: any
 }
-
-const finalWishData = [
-  {
-    id: "1",
-    value: "wish1",
-    label: "Wish 1"
-  },
-  {
-    id: "2",
-    value: "wish2",
-    label: "Wish 2"
-  },
-  {
-    id: "3",
-    value: "wish3",
-    label: "Wish 3"
-  }
-]
 
 export function Step4({ form }: Props) {
   // const { classes } = useStyles()
---
+++ src/components/MultiStepForm/Step4/data.ts
@@ -0,0 +1,17 @@
+export const finalWishData = [
+    {
+        id: "1",
+        value: "wish1",
+        label: "Wish 1"
+    },
+    {
+        id: "2",
+        value: "wish2",
+        label: "Wish 2"
+    },
+    {
+        id: "3",
+        value: "wish3",
+        label: "Wish 3"
+    }
+]
