79c6972eb3f66f2bed54946670cb76589311d487
GotPop <gotpop@users.noreply.github.com>
2023-03-02 15:20:39 +0000

    `Task:` Faqs structure

--- src/pages/faqs.tsx
+++ src/pages/faqs.tsx
@@ -1,12 +1,11 @@
 import { FaqsBlock } from "@/components/FaqsBlock"
 import { Layout } from "@/components/Layout"
-import { imgPaths } from "@/utils/imgPaths"
-import { Accordion, Col, Container, createStyles, Grid, MantineTheme, Title } from "@mantine/core"
+import { Title } from "@mantine/core"
 
 export default function Faqs() {
   return (
     <Layout>
-      <Title order={1}>Frequently Asked Questions</Title>
+      <Title order={2}>Frequently Asked Questions</Title>
       <FaqsBlock />
     </Layout>
   )
