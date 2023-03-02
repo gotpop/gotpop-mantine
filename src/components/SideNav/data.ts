4fba55c6a7c3057d42b886deb96f55e70bb1c4ac
GotPop <gotpop@users.noreply.github.com>
2023-03-02 18:20:45 +0000

    `Enhancement:` Rename page

--- src/components/SideNav/data.ts
+++ src/components/SideNav/data.ts
@@ -1,6 +1,6 @@
 import { IconRobot, IconRocket } from "@tabler/icons-react";
 
 export const dataMenu = [
-    { id: 1, href: "/mission-control/onboarding", label: "Onboarding", Icon: IconRocket },
+    { id: 1, href: "/mission-control/preflight", label: "Preflight", Icon: IconRocket },
     { id: 2, href: "/mission-control/profile", label: "Profile", Icon: IconRobot }
 ]
