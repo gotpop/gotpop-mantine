3919f1268cb94225fcbd0f39879d8031bc360933
GotPop <gotpop@users.noreply.github.com>
2023-03-06 00:27:54 +0000

    `Enhancement:` Updating login add redirect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,13 @@
 import { AuthenticationForm } from "@/components/AuthenticationForm"
 import { Layout } from "@/components/Layout"
 import { Container } from "@mantine/core"
+import { GetServerSideProps } from "next"
+import { getServerSession, Session } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]"
+
+type Props = {
+  sessionData: Session
+}
 
 export default function Login() {
   return (
@@ -10,4 +17,23 @@ export default function Login() {
       </Container>
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/mission-control",
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {
+      sessionData: session // NextAuth session is namespaced. Use any other variable name.
+    }
+  }
 }
