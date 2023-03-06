e6a41a952403c7c72bec8457ea412f00c81685da
GotPop <gotpop@users.noreply.github.com>
2023-03-06 00:37:57 +0000

    `Enhancement:` Updating login add redirect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,17 @@
 import { AuthenticationForm } from "@/components/AuthenticationForm"
 import { Layout } from "@/components/Layout"
 import { Container } from "@mantine/core"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
 
 export default function Login() {
+  const { status } = useSession()
+  const router = useRouter()
+
+  if (status === "authenticated") {
+    router.push("/mission-control")
+  }
+
   return (
     <Layout>
       <Container py="xl" mt={100}>
